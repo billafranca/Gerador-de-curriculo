1ca570335eca192a4afeb1c6767156261e79379c
PEDRO_5245 <PEDRO_5245@uirapuru.br>
2025-08-28 19:27:06 -0300

    front

---
+++ projeto_sistema/front-end/page_explicativa/js/index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let btn = document.getElementById("botaoCadastro");
+
+    btn.addEventListener("click", function() {
+        window.location.href = "../login/loginOne.html";
+    })
+
+});
