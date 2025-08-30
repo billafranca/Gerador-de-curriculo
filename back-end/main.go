07dfa555f23f54d15eb2a586247e2736b35e1284
Lucas Rabello <lucasrabello2020@gmail.com>
2025-08-30 18:12:52 -0300

    Rodou o back

--- back-end/main.go
+++ back-end/main.go
@@ -35,9 +35,35 @@ func main() {
 
 	// /static/ -> é um prefixo da rota que você configura no Go para servir arquivos static 
 
+	// Servir estáticos (css, js, imagens etc.)
+	fs := http.FileServer(http.Dir("../projeto_sistema/front-end/login"))
+	http.Handle("/login/", http.StripPrefix("/login/", fs))
+
+	// Página inicial
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "../projeto_sistema/front-end/login/loginOne.html")
 	})
+
+	// Rota para processar o formulário
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			// Process form data
+			r.ParseForm()
+			nome := r.FormValue("nome")
+			email := r.FormValue("email")
+			senha := r.FormValue("senha")
+
+			// Aqui você pode fazer o que quiser com os dados, como salvá-los no banco de dados
+			fmt.Fprintf(w, "Nome: %s\nEmail: %s\nSenha: %s\n", nome, email, senha)
+		} else {
+			http.ServeFile(w, r, "../projeto_sistema/front-end/login/loginOne.html")
+		}
+	})
+
+	// fs2 := http.FileServer(http.Dir("../projeto_sistema/front-end/page_explicativa/index.html"))
+	// http.Handle("/page_explicativa/", http.StripPrefix("/page_explicativa/", fs2))
+
+
 
 	fmt.Println("Servidor rodando em http://localhost:8000")
 	log.Fatal(http.ListenAndServe(PORTA, nil)) // -> server mux está como nil
