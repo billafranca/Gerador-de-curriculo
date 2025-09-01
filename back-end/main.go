8dd7f0d7b1ab9c47183ed218c39ce900d00c9a20
Lucas Rabello <lucasrabello2020@gmail.com>
2025-09-01 11:39:51 -0300

    Update essa merda

--- back-end/main.go
+++ back-end/main.go
@@ -20,29 +20,32 @@ func main() {
 
     // Servir estáticos (css, js, imagens etc.)
     fs := http.FileServer(http.Dir("../projeto/front-end/page_login"))
-    mux.Handle("/page_login/", http.StripPrefix("/page_login/", fs))
+    mux.Handle("/login/", http.StripPrefix("/login/", fs))
 
     // Página inicial redireciona para /login
-    mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        http.Redirect(w, r, "/login", http.StatusFound)
-    })
-
-    // Rota para processar o formulário e servir a página de login
     mux.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-        if r.Method == http.MethodPost {
-            // Process form data
-            r.ParseForm()
-            nome := r.FormValue("nome")
-            email := r.FormValue("email")
-            senha := r.FormValue("senha")
-
-            // Aqui você pode fazer o que quiser com os dados, como salvá-los no banco de dados
-            fmt.Fprintf(w, "Nome: %s\nEmail: %s\nSenha: %s\n", nome, email, senha)
-        } else {
-            http.ServeFile(w, r, "../projeto/front-end/page_login/loginOne.html")
-        }
+        http.ServeFile(w, r, "../projeto/front-end/page_login/index.html")
     })
+
+    // // Rota para processar o formulário e servir a página de login
+    // mux.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+    //     if r.Method == http.MethodPost {
+    //         // Process form data
+    //         r.ParseForm()
+    //         nome := r.FormValue("nome")
+    //         email := r.FormValue("email")
+    //         senha := r.FormValue("senha")
+
+    //         // Aqui você pode fazer o que quiser com os dados, como salvá-los no banco de dados
+    //         fmt.Fprintf(w, "Nome: %s\nEmail: %s\nSenha: %s\n", nome, email, senha)
+    //     } else {
+    //         http.ServeFile(w, r, "../projeto/front-end/page_login/loginOne.html")
+    //     }
+    // })
 
     fmt.Println("Servidor rodando em http://localhost:8000")
-    log.Fatal(http.ListenAndServe(PORTA, mux)) // Passa o ServeMux como argumento
+    err := http.ListenAndServe(PORTA, mux) // Passa o ServeMux como argumento
+    if err != nil {
+        log.Fatal(err)
+    }
 }
