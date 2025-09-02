0c85064df31c4b1d49b957aec80ab62a85a45223
PEDRO_5245 <PEDRO_5245@uirapuru.br>
2025-09-02 20:51:12 -0300

    banco funcionando com sqlite

--- projeto/front-end/page_login/js/loginOne.js
+++ projeto/front-end/page_login/js/loginOne.js
@@ -35,4 +35,49 @@ document.getElementById('formulario').addEventListener('submit', function(event)
         console.error('Erro:', error);
         alert('Erro ao cadastrar usuário!');
     });
+
+
+
+    document.addEventListener('DOMContentLoaded', function () {
+        let homeLink = document.getElementById('home');
+        let sobreLink = document.getElementById('sobre');
+        let contatoLink = document.getElementById("contato");
+    
+       
+    
+        homeLink.addEventListener('click', function (event) {
+            event.preventDefault();
+            alert('redirecionando para a home page ...');
+            window.location.href = '../../page_entrada/page_entrada.html';
+        });
+        sobreLink.addEventListener('click', function (event) {
+            event.preventDefault();
+            window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+        });
+    
+    
+        contatoLink.addEventListener('click', function (event) {
+        event.preventDefault();
+    
+        let escolha = prompt(
+            "Entre em contato com os desenvolvedores do projeto:\n" +
+            "Digite 1 para Pedro Billafranca\n" +
+            "Digite 2 para Lucas Rabello"
+        );
+    
+        if (!escolha) return;
+    
+        escolha = escolha.trim();
+    
+        if (escolha === "1") {
+            alert("Redirecionando para o GitHub de Pedro Billafranca...");
+            window.location.href = "https://github.com/billafranca";
+        } else if (escolha === "2") {
+            alert("Redirecionando para o GitHub de Lucas Rabello...");
+            window.location.href = "https://github.com/lucas-rabello-dev";
+        } else {
+            alert("Opção inválida. Tente novamente.");
+        }})
+    
+    });
 });
